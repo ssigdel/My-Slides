89bdf444bbcd582b7c4674ed3813e4b4d4e193a6
unknown <shirishsigdel26@gmail.com>
2022-01-10 08:42:31 +0545

    added screen state

---
+++ src/state.js
@@ -0,0 +1,29 @@
+let screenState = {
+    homeScreen : true,
+    slideScreen: false
+}
+
+
+if(screenState.homeScreen){
+    mainSection.style.display = 'none'
+    leftSection.style.display = 'none'
+    navBar.style.display = 'none'
+    presentButton.style.display= 'none'
+    header.style.borderBottom = '0'
+}
+
+
+blankSlide.addEventListener('click', (e) => {
+    console.log('clicked')
+    screenState.homeScreen = false
+    screenState.slideScreen = true
+
+    if(screenState.slideScreen){
+        blankSlide.style.display = 'none'
+        mainSection.style.display = 'flex'
+        leftSection.style.display = 'block'
+        navBar.style.display = 'flex'
+        presentButton.style.display= 'block'
+        header.style.borderBottom = '1px solid #999'
+    }
+})
